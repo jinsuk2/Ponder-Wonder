62ba5eba8daba74d9244fb20d1d71adf72a775c3
Jinsuk Kim <jinsukkim94@gmail.com>
2019-06-12 15:02:23 -0700

    Popping stash

--- app/src/main/java/com/example/ponderwonder/SignIn.java
+++ app/src/main/java/com/example/ponderwonder/SignIn.java
@@ -34,32 +34,4 @@ public class SignIn extends Fragment {
         return inflater.inflate(R.layout.fragment_sign_in, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-//
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
-
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
 }
