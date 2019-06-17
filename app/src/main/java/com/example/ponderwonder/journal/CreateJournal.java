46376af7e88f50c5f7aff9c1866e112992968836
Paul <paulgmkim@gmail.com>
2019-06-17 15:32:41 -0700

    date dialog added from create journal

--- app/src/main/java/com/example/ponderwonder/journal/CreateJournal.java
+++ app/src/main/java/com/example/ponderwonder/journal/CreateJournal.java
@@ -1,27 +1,57 @@
 package com.example.ponderwonder.journal;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.ponderwonder.R;
 
+import java.util.Calendar;
+
 
 public class CreateJournal extends Fragment {
+
+    DatePickerDialog journalDatePicker;
+    ImageButton dateButton;
+    TextView tempJournalDate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_create_journal, container, false);
+        tempJournalDate = view.findViewById(R.id.new_date);
+        dateButton = view.findViewById(R.id.date_button);
 
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                journalDatePicker = new DatePickerDialog(getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                tempJournalDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                journalDatePicker.show();
+            }
+        });
 
         return view;
     }
